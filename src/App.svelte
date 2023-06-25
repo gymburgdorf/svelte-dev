786735a864660c33237ea223688fe4d3d5890d3d
gymburgdorf <adrian.luethi@gymburgdorf.ch>
2023-06-25 17:01:03 +0200

    init

---
+++ src/App.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  import svelteLogo from './assets/svelte.svg'
+  import viteLogo from '/vite.svg'
+  import Button from './ui-comps/Button.svelte';
+
+  let count = 0
+</script>
+
+<main>
+  <Button on:click={()=>count = count+1}>Count is {count}</Button>
+  <div>
+    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+      <img src={viteLogo} class="logo" alt="Vite Logo" />
+    </a>
+    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
+      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
+    </a>
+  </div>
+</main>
+
+<style>
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em #ff3e00aa);
+  }
+</style>
---
+++ src/ui-comps/Button.svelte
@@ -0,0 +1,19 @@
+<script>
+
+</script>
+
+<button on:click>
+	<slot></slot>
+</button>
+
+<style>
+	button {
+		padding: 1em;
+		background-color: #8888;
+		will-change: filter;
+    transition: filter 300ms;
+	}
+ button:hover {
+    filter: drop-shadow(0 0 5em #64ffffcc);
+  }
+</style>
